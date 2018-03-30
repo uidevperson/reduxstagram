7bcc2180fe9c2fef0972dc85d95e78c6f4c4ed9d
uidevperson <uidevthing@gmail.com>
2018-03-30 20:16:33 +0100

    import comp

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,7 +1,10 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
-import Main from './components/Main'
+import Main from './components/Main';
+// import components
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 import css from './styles/style.styl';
 
 render(<Main />, document.getElementById('root'));
