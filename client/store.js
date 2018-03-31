2644132747681bc8599c7d4a50c20fc4dc73b3c4
uidevperson <uidevthing@gmail.com>
2018-03-31 19:53:22 +0100

    SETUP

--- client/store.js
+++ client/store.js
@@ -13,7 +13,6 @@ const defaultState = {
 };
 
 const store = createStore(rootReducer, defaultState);
-
 export const history = syncHistoryWithStore(browserHistory, store);
-
 export default store;
+ 
