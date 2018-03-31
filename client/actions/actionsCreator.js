c2306c1cf7bf205399dfd3e81de8f5cce74bc675
uidevperson <uidevthing@gmail.com>
2018-03-31 20:14:19 +0100

    add functions

--- client/actions/actionsCreator.js
+++ client/actions/actionsCreator.js
@@ -1,7 +1,24 @@
 // action creator
 
 // increment
-
+export function increment(index){
+	return {
+		type: 'INCREMENT_LIKES',
+		index
+	}
+}
 // add comment
- 
+export function addComment(postID, author, comment){
+	return {
+		type: 'ADD_COMMENT',
+		author,
+		comment
+	}
+}
 // remove comment
+export function removeComment(postID, index){
+	return {
+		type: 'REMOVE_COMMENT',
+		index
+	}
+}
