2071e15dbc490d6a3f488edc32870663f355b5b6
uidevperson <uidevthing@gmail.com>
2018-04-01 17:01:12 +0100

    posts reducer

---
+++ client/reducers/posts.js
@@ -0,0 +1,9 @@
+// a reducer takes in  2 things
+
+// 1. the action (what happened)
+
+// 2. takes in a copy of the current start
+
+function posts(state=[], action){
+	console.log(state, action);
+}
