a018ca4d1a7f7577550b910d2f9d5524206b1a46
uidevperson <uidevthing@gmail.com>
2018-04-01 17:10:54 +0100

    routeReducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,1 +1,16 @@
 // index.js
+import { combineReduxers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+//
+import posts from './posts';
+import comments from './comments';
+
+const routerReducer = combineReduxers({
+	posts,
+	comments,
+	route: routerReducer
+});
+
+export default routerReducer;
+
+
