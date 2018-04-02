dce5761513006e20fbf06e6bea0202fc2eff3419
uidevperson <uidevthing@gmail.com>
2018-04-02 20:55:49 +0100

    comment

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,9 @@
 // index.js
 import { combineReduxers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-//
 import posts from './posts';
 import comments from './comments';
-
+//
 const routerReducer = combineReduxers({
 	posts,
 	comments,
