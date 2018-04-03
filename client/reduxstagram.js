37aca7a96f9174a3dfd98949b94a5bb37b957dbb
uidevperson <uidevthing@gmail.com>
2018-04-03 20:03:45 +0100

    provider

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -12,6 +12,8 @@ import PhotoGrid from './components/PhotoGrid';
 
 // import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+// import store, { history } from './store';
 
 const router = (
   <Router history={browserHistory}>
