8bdb3131b7f4acee8e6158596a23d2fa0f7597be
uidevperson <uidevthing@gmail.com>
2018-04-03 20:12:01 +0100

    bugfix

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,15 +1,15 @@
 // index.js
-import { combineReduxers } from 'redux';
+import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import posts from './posts';
 import comments from './comments';
 //
-const RouterReducer = combineReduxers({
+const rootReducer = combineReducers({
 	posts,
 	comments,
 	routing: routerReducer
 });
 
-export default RouterReducer;
+export default rootReducer;
 
 
