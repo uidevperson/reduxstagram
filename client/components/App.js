ad0dfe2a719ebca7c82d098d251ce64e4b66bee7
uidevperson <uidevthing@gmail.com>
2018-04-05 19:45:40 +0100

    connect

--- client/components/App.js
+++ client/components/App.js
@@ -1,5 +1,20 @@
 import { bindActionCreators} from 'redux';
 import { connect } from 'react-redux';
-import * as { actionCreators } from '../actions/actionCreator';
+import * as actionCreators from '../actions/actionsCreator';
 //
 import Main from './Main';
+
+function mapStateToProps(state){
+	return {
+		posts: state.posts,
+		comments: state.comments
+	}
+}
+
+function mapDispatchTpProps(dispatch){
+	return bindActionCreators(actionCreators, dispatch);
+}
+
+const App = connect(mapStateToProps, mapDispatchTpProps)(Main);
+
+export default App;
