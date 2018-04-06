6746b7db5ef1fdbd23db6d2b937826872dcb91ce
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-04-06 16:38:04 +0100

    Add files via upload

---
+++ client/components/Photo.js
@@ -0,0 +1,15 @@
+//Photo
+ import React from 'react';
+
+
+ const Photo = React.createClass({
+ 	render(){
+ 		return (
+ 			<figure className="grid-figure">
+ 				I am a Photo
+ 			</figure>
+ 		)
+ 	}
+ })
+
+ export default Photo;
--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Photo from './Photo';
 //
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
       	<pre>
-      		{JSON.stringify(this.props.posts, null, '')}
+      		{this.props.posts.map((psot, i) => <Photo {...this.props} key={i} i={i}/>)}
       	</pre>
       </div>
     )
